e990d6941a6fe9d3336bf995a61653c963c7875a
capitan <capitan@capitans-Mac.local>
2016-10-10 17:33:00 +0300

    .gitignore

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -5,9 +5,15 @@ RSpec.describe "Project with configuration" do
     run_app_generator
   end
 
-  describe "Custom README.md" do
+  describe "README.md" do
     subject { IO.read("#{project_path}/README.md") }
 
     it { is_expected.to match(/Requirements/)}
+  end
+
+  describe ".gitignore" do
+    subject { IO.read("#{project_path}/.gitignore") }
+
+    it { is_expected.to be_truthy }
   end
 end
