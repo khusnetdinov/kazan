59c1b7cf6975e3e8a06c14a68bd40ca368e070f9
Marat Khusnetdinov <khusnetdinov@users.noreply.github.com>
2016-10-18 11:09:01 +0300

    Update spec_helper.rb

--- templates/spec_helper.rb
+++ templates/spec_helper.rb
@@ -18,7 +18,7 @@ RSpec.configure do |config|
   config.example_status_persistence_file_path = 'tmp/rspec_examples.txt'
   config.order = :random
 
-  config.infer_spec_type_from_file_location!
+   # config.infer_spec_type_from_file_location!
 end
 
 WebMock.disable_net_connect!(allow_localhost: true)
