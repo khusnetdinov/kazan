21dc0fadaf7db9e8864836ad82a03002cb4bb31b
Marat Khusnetdinov <marat@khusnetdinov.ru>
2016-11-27 18:37:44 +0300

    version

--- lib/kazan/version.rb
+++ lib/kazan/version.rb
@@ -1,5 +1,5 @@
 module Kazan
-  VERSION = '0.2.4'.freeze
+  VERSION = '0.3.0'.freeze
   RAILS_VERSION = '~> 5.0.0'.freeze
   RUBY_PROJECT_VERSION = IO.read("#{File.dirname(__FILE__)}/../../.ruby-version").strip.freeze
 end
