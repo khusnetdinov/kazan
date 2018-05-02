a63a059d5c224378df7d5878b0790f445f29878e
Marat Khusnetdinov <marat@khusnetdinov.ru>
2018-05-02 12:10:31 +0300

    rails-i18n

--- lib/kazan/version.rb
+++ lib/kazan/version.rb
@@ -1,5 +1,6 @@
 module Kazan
   VERSION = '0.5.0'.freeze
-  RAILS_VERSION = '~> 5.2.0'.freeze
+  RAILS_VERSION = '5.2.0'.freeze
+  RAILS_I18N_VERSION = '~> 5.1'.freeze
   RUBY_PROJECT_VERSION = IO.read("#{File.dirname(__FILE__)}/../../.ruby-version").strip.freeze
 end
