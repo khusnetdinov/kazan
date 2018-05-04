c448854e9e9a7922f0dc882b965774e16146f3cd
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2018-05-04 16:02:15 +0300

    (#17) update test settings_utility_spec.rb

--- templates/seo/settings_utility_spec.rb
+++ templates/seo/settings_utility_spec.rb
@@ -4,14 +4,21 @@ require 'rails_helper'
 
 RSpec.describe SettingsUtility do
   describe '.site_url' do
+    let(:actual) { SettingsUtility.site_url }
+    let(:result) { 'localhost' }
 
+    it { expect(actual).to eq(result) }
   end
 
   describe '.site_allowed' do
+    let(:actual) { SettingsUtility.site_allowed.include?('/robots.txt') }
 
+    it { expect(actual).to be_truthy }
   end
 
   describe '.site_disallowed' do
+    let(:actual) { SettingsUtility.site_disallowed }
 
+    it { expect(actual).to be_falsey }
   end
 end
